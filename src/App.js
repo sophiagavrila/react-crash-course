3609b14d3283dae71628ad800e0a100f8d07e3e6
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-01-29 10:44:25 -0500

    describe the breakdown of App.js component, index.js, and index.html root'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+// This is the first React Component that we've created - selected by <App /> and displayed in index.js
 function App() {
+  // this is a regular JS function, other than the fact that it renders an HTML element
   return <div>Hello!</div>;
+  // A Component must return something that can be rendered in the browser. (Accomplisehd with JSX code)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom'; 
 
-ReactDOM.render(<App />, document.getElementById('root')
+import './index.css';
+import App from './App'; // .js can be hidden from JS files
+
+// index.js == starting point of React JS app (first code that's loaded by the browser)
+ReactDOM.render(<App />, document.getElementById('root') // root points to index.html
+// <App /> is JSX.  Not understood by the browser but a React template that's passed as HTML element
 );
