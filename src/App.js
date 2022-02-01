bf75b000bede2d5cc2f817e168a571e8feb146af
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-01 10:30:30 -0500

    setup MeetupApp and make pages directory with 3 components - install router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1> 
-      <Todo text='Learn React' />
-      <Todo text='Fold Socks' />
-      <Todo text='Beat Dark Souls' />
-      {/* Remove Modal and Backdrop since we're not using it here (in Todo only) */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 
 import './index.css';
-import App from './App'; // .js can be hidden from JS files
+import App from './App';
 
-// index.js == starting point of React JS app (first code that's loaded by the browser)
-ReactDOM.render(<App />, document.getElementById('root') // root points to index.html
-// <App /> is JSX.  Not understood by the browser but a React template that's passed as HTML element
+ReactDOM.render(<App />, document.getElementById('root') 
 );
