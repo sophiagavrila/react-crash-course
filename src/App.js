8b3014ce380f196658061011c5af6e24f1d26876
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-01 10:42:16 -0500

    10 add Route (with nested content) to display each component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+// The job of the Route component is to define different paths in the URL and which component should be loaded for these diff paths.
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
-  return <div></div>;
+  // localhost:3000/ = domain..part afterwards is the path like /products, /favorites, etc...
+
+  return(
+    <div>
+      <Route path='/'>
+        <AllMeetupsPage /> {/* default component to display  */}
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route> {/* We see the "Nested" output with the AllMeetupsPage ALWAYS rendering because any other component includes it */}
+    </div>
+  );
 }
 
 export default App;
